511756c9724712ecad22e2fb37116fd9643de40c
최재민 <admin@chicken-moo.com>
2024-04-20 22:32:49 +0900

    feat: docs button

--- src/app/(login)/layout.tsx
+++ src/app/(login)/layout.tsx
@@ -50,6 +50,7 @@ const LoginedLayout = async ({
         <article className="w-full flex flex-col justify-center items-center gap-2">
           <p className="text-text/40 text-sm">&#34;내가 이것보단 잘 만들겠다 ㅋㅋ&#34;하는 경우엔 <a className="text-primary/40 underline" href="https://github.com/jeamxn/dimigoin-pull-service" target="_blank" rel="noreferrer">직접 만드세요</a>!</p>
           <p className="text-text/40 text-sm">오류 및 기타 문의 사항은 <a className="text-primary/40 underline" href="kakaoopen://join?l=%2Fme%2FJeamxn&r=EW">최재민</a>에게 연락바랍니다!</p>
+          <p className="text-text/40 text-sm">자세한 서비스 사용 방법은 <a className="text-primary/40 underline" href="https://docs.dimigo.net" target="_blank" rel="noreferrer">여기</a>를 참고 해주세요!</p>
         </article>
       </footer>
     </>
