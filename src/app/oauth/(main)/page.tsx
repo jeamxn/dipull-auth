5901c48954b90fcf93cad59140ffb86b1df2a75c
Jeamin Choi <admin@chicken-moo.com>
2024-06-23 11:08:43 +0900

    design: 디풀 계정 센터 -> Dipull Auth (#26)

--- src/app/oauth/(main)/page.tsx
+++ src/app/oauth/(main)/page.tsx
@@ -52,9 +52,25 @@ const Oauth = async ({
 
     return (
       <Insider className="flex flex-col items-center justify-center h-full">
-        <div className="bg-white rounded-xl p-6 flex flex-col gap-6 w-full max-w-[26rem]">
+        <div className="bg-white rounded-xl p-6 flex flex-col gap-6 w-full max-w-[26rem] border border-text/5">
           <div className="flex flex-col gap-1">
-            <p className="text-sm text-text/50">디풀 계정 센터로</p>
+            <div className="flex flex-row gap-0 items-center">
+              <div className="flex flex-row gap-1 items-center">
+                <svg width="14" height="14" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <g clipPath="url(#clip0_1115_161)">
+                    <path className="fill-text/50" d="M26.8382 18.3641C27.7759 19.3018 29.0477 19.8286 30.3738 19.8286H55.1723C57.9338 19.8286 60.1724 22.0671 60.1724 24.8286V49.6272C60.1724 50.9533 60.6991 52.2251 61.6368 53.1628L71.4654 62.9913C74.6152 66.1411 80.0009 63.9103 80.0009 59.4558V5C80.0009 2.23857 77.7623 0 75.0009 0H20.5452C16.0907 0 13.8598 5.38571 17.0097 8.53553L26.8382 18.3641Z" />
+                    <path className="fill-text/50" d="M53.1628 61.6368C52.2251 60.6991 50.9533 60.1724 49.6272 60.1724H24.8286C22.0672 60.1724 19.8286 57.9338 19.8286 55.1724V30.3738C19.8286 29.0477 19.3018 27.7759 18.3641 26.8382L8.53554 17.0097C5.38572 13.8598 0 16.0907 0 20.5452V75.0009C0 77.7624 2.23858 80.0009 5 80.0009H59.4558C63.9103 80.0009 66.1412 74.6152 62.9913 71.4654L53.1628 61.6368Z" />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_1115_161">
+                      <rect width="80" height="80" fill="white"/>
+                    </clipPath>
+                  </defs>
+                </svg>
+                <p className="text-sm text-text/50 font-semibold">Dipull Auth</p>
+              </div>
+              <p className="text-sm text-text/50">을 통해</p>
+            </div>
             <div className="flex flex-row text-xl font-semibold">
               <p className="text-primary">{clientData.name}</p>
               <p>에 로그인</p>
